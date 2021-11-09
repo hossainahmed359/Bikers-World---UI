a9623b1040912679d7fae3f605e7560462c407cf
hossainahmed359 <hossainahmed359@gmail.com>
2021-11-10 03:29:22 +0600

    material ui installed

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
+import { Button } from '@mui/material';
 import React from 'react';
 
 const Home = () => {
     return (
         <div>
             <h1>This is home</h1>
+            <Button variant="contained">Hello WOrld</Button>
         </div>
     );
 };
