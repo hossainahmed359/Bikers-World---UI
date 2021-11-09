09998c352b5cdd6202b80648d8e7bec7695275b0
hossainahmed359 <hossainahmed359@gmail.com>
2021-11-10 03:14:27 +0600

    react-router v5 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Explore from './Pages/Explore/Explore';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/explore">
+            <Explore></Explore>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
