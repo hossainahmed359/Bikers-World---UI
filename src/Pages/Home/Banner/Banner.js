c5fc912b722376948941a27138925ad4591e3f54
hossainahmed359 <hossainahmed359@gmail.com>
2021-11-10 15:19:55 +0600

    banner complete

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import './Banner.css'
 
+// This css style is a major part of this componet
 const bannerStyles = {
     background: `url('https://i.ibb.co/0YRdZ4n/Bike-Banner.jpg')`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: '100%',
-    height: '850px'
+    height: '850px',
 }
+
 
 const Banner = () => {
     return (
         <div style={bannerStyles}>
-            <Container>
-                <Row>
-                    <Col xs={12} md={6} lg={6}>
-                        <h1 className="text-light">This is lefts</h1>
-                    </Col>
-                    <Col xs={12} md={6} lg={6}>
-                        <h1 className="text-light">This is right</h1>
+            <Container className="alignment">
+                <Row className="banner-item pt-3">
+                    <Col xs={12} md={12} lg={8} className="d-flex" >
+                        <div className="text-start">
+                            <h1 className="text-light innerItemsStyle">NOT ALL WHO</h1>
+                            <h1 className="text-light innerItemsStyle" >WANDER</h1>
+                            <h1 className="innerItemsStyle" style={{ color: '#E52727 ' }}>ARE LOST !</h1>
+                        </div>
                     </Col>
                 </Row>
             </Container>
-        </div>
+        </div >
     );
 };
 
