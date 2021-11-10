b21e8d32bdf0d5852cda9808ab5aa939b9903950
hossainahmed359 <hossainahmed359@gmail.com>
2021-11-10 22:53:15 +0600

    firebase initialization and context api development started

--- src/App.js
+++ src/App.js
@@ -6,33 +6,36 @@ import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Purchase from './Pages/Purchase/Purchase';
 import Dashboard from './Pages/Dashboard/Dashboard/Dashboard';
+import AuthProvider from './contexts/AuthProvider/AuthProvider';
 
 
 function App() {
   return (
     <div className="App text-fonts">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route path="/explore">
-            <Explore></Explore>
-          </Route>
-          <Route path="/purchase/:_id">
-            <Purchase></Purchase>
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard></Dashboard>
-          </Route>
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="/explore">
+              <Explore></Explore>
+            </Route>
+            <Route path="/purchase/:_id">
+              <Purchase></Purchase>
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard></Dashboard>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,131 @@
+import { getAuth, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useEffect, useState } from "react";
+import initializeFirebse from "../Pages/Login/Firebase/firebase.initialize";
+
+//initialization
+initializeFirebse();
+
+// firebase functionalities
+const useFirebase = () => {
+
+
+    // States
+    const [user, setUser] = useState({});
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    // AUTH
+    const auth = getAuth();
+
+
+    // Provider
+    const googleProvider = new GoogleAuthProvider();
+
+
+    // Google Sign In
+    const handleGoogleSignIn = () => {
+        setIsLoading(true);
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                setError(null);
+                // const user = result.user;
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                setError(error.message);
+            })
+            .finally(() => setIsLoading(false));
+    }
+
+
+    // Email Registration and Update Name
+    const handleEmailRegistration = (name, email, password) => {
+        setIsLoading(true);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                setError(null)
+                // Checking registration success 
+                if (result.email) {
+                    // Updating Profile
+                    updateProfile(auth.currentUser, {
+                        displayName: name
+                    }).then(() => {
+                        // Setting User Name on Update Success || Not waiting for observer
+                        const newUser = { email: email, displayName: name }
+                        setUser(newUser)
+                        //
+                    }).catch((error) => {
+                        setError(error.message)
+                    })
+                }
+            })
+            .catch((error) => {
+                setError(error.message);
+            })
+            .finally(() => setIsLoading(false))
+    }
+
+
+    // Email Sign In
+    const handleEmailSignIn = (email, password) => {
+        setIsLoading(true);
+        signInWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                setError(null)
+                // Sign in success
+                // const user = result.user;
+
+            })
+            .catch((error) => {
+                setError(error.message);
+            })
+            .finally(() => { setIsLoading(false) });
+    }
+
+
+    // Sign Out
+    const handleSignOut = () => {
+        setIsLoading(true);
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            setUser({});
+            setError(null);
+        }).catch((error) => {
+            // An error happened.
+            setError(error.message)
+        })
+            .finally(() => { setIsLoading(false) });
+    }
+
+
+    // Observer || User Login || User Logout
+    useEffect(() => {
+        setIsLoading(true)
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user)
+                // ...
+            } else {
+                // User is signed out
+                setUser({})
+            }
+            setIsLoading(false);
+        });
+    }, []);
+
+
+    // Return
+
+    return {
+        user,
+        error,
+        isLoading,
+        handleGoogleSignIn,
+        handleEmailRegistration,
+        handleEmailSignIn,
+        handleSignOut
+    }
+}
+
+export default useFirebase;
