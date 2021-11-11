8cdc0af93a503cc9491f2c4cd9e6575ae13d626d
hossainahmed359 <hossainahmed359@gmail.com>
2021-11-11 16:10:11 +0600

    detail infomation form under development

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -3,6 +3,7 @@ import { Card, Col, Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import useAuth from '../../Hooks/useAuth';
 import Navigation from '../Shared/Navigation/Navigation';
+import UserInfo from './UserInfo/UserInfo';
 
 const Purchase = () => {
     const { product_id } = useParams();
@@ -17,9 +18,6 @@ const Purchase = () => {
             .then(res => res.json())
             .then(data => setSingleProduct(data))
     }, []);
-
-    console.log(singleProduct);
-    console.log(user)
 
 
 
@@ -42,17 +40,11 @@ const Purchase = () => {
                             </Card>
                         </Col>
                         <Col sm={12} md={6} lg={8}>
-                            <Col>
-                                <Card className="border-0">
-                                    <Card.Img variant="top" src="holder.js/100px160" />
-                                    <Card.Body>
-                                        <Card.Title>Card title</Card.Title>
-                                        <Card.Text style={{ textAlign: 'justify' }}>
-                                            This is a longer card with supporting text below as a natural
-                                            lead-in to additional content. This content is a little bit longer.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
+                            <Col className="mx-auto" lg={10}>
+                                <UserInfo
+                                    user={user}
+                                    singleProduct={singleProduct}
+                                ></UserInfo>
                             </Col>
                         </Col>
 
---
+++ src/Pages/Purchase/UserInfo/UserInfo.js
@@ -0,0 +1,87 @@
+import { fontFamily } from '@mui/system';
+import React from 'react';
+import { Card, FormControl, InputGroup, Button } from 'react-bootstrap';
+
+const UserInfo = ({ user, singleProduct }) => {
+
+    console.log(singleProduct);
+    console.log(user);
+
+
+    const handleOnBlur = e => {
+        // console.log(e.targe.value)
+    }
+
+    return (
+        <div className="mx-auto">
+            <Card
+                className="border-0">
+                <Card.Body>
+                    <Card.Title className="fs-1">User Details</Card.Title>
+                    <form style={{ fontFamily: 'roboto' }}>
+                        <InputGroup className="my-4">
+                            <FormControl
+                                className="py-3"
+                                onBlur={handleOnBlur}
+                                defaultValue={user.displayName}
+                                name="name"
+                                type="text"
+                                placeholder="Your Name"
+                                aria-label="name"
+                                aria-describedby="basic-addon1"
+                                required
+                            />
+                        </InputGroup>
+                        <InputGroup className="my-4">
+                            <FormControl
+                                className="py-3"
+                                onBlur={handleOnBlur}
+                                defaultValue={user.email}
+                                name="email"
+                                type="email"
+                                placeholder="Your Email"
+                                aria-label="email"
+                                aria-describedby="basic-addon1"
+                                required
+                            />
+                        </InputGroup>
+                        <InputGroup className="my-4">
+                            <FormControl
+                                className="py-3"
+                                onBlur={handleOnBlur}
+                                name="number"
+                                type="number"
+                                placeholder="Your Phone Number"
+                                aria-label="password"
+                                aria-describedby="basic-addon1"
+                                required
+                            />
+                        </InputGroup>
+                        <InputGroup className="my-4">
+                            <FormControl
+                                className="py-3"
+                                onBlur={handleOnBlur}
+                                name="address"
+                                type="text"
+                                placeholder="Your Loaction"
+                                aria-label="address"
+                                aria-describedby="basic-addon1"
+                                required
+                            />
+                        </InputGroup>
+
+                        <Button
+                            type="submit"
+                            variant="danger"
+                            className="w-100 px-3 py-3 my-2 border rounded-pill"
+                        >
+                            Add To Cart
+                        </Button>
+                    </form>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default UserInfo;
