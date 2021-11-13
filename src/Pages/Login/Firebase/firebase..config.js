4a3f9022d5d0016245865341d751428fd3062b36
hossainahmed359 <hossainahmed359@gmail.com>
2021-11-13 15:58:59 +0600

    firebase config undone

--- src/Pages/Login/Firebase/firebase..config.js
+++ src/Pages/Login/Firebase/firebase..config.js
@@ -1,4 +1,4 @@
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCLj1TgdJYP07NyfD4nkO45rLrYVUsGClY",
     authDomain: "bikers-world-9dc58.firebaseapp.com",
@@ -9,15 +9,3 @@ const firebaseConfig = {
 };
 
 export default firebaseConfig;
-
-/*
-const firebaseConfig = {
-    apiKey: "AIzaSyCLj1TgdJYP07NyfD4nkO45rLrYVUsGClY",
-    authDomain: "bikers-world-9dc58.firebaseapp.com",
-    projectId: "bikers-world-9dc58",
-    storageBucket: "bikers-world-9dc58.appspot.com",
-    messagingSenderId: "220687215403",
-    appId: "1:220687215403:web:3d91e67623d52640cb736e"
-};
-
-*/
